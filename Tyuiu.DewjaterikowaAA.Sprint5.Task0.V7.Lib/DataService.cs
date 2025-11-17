4179356464b8ba800afb9c26b7db47a9b50b3bef
Anastasia24057 <nastya.dewa@gmail.com>
2025-11-17 21:07:02 +0500

    Add project files.

---
+++ Tyuiu.DewjaterikowaAA.Sprint5.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint5.Task0.V7.Lib
+{
+    public class DataService : ISprint5Task0V7
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+        
+            double f = -Math.Pow(x, 3) + 4 * Math.Pow(x, 2) - 1.5 * x;
+            f = Math.Round(f, 3);
+            File.WriteAllText(path, Convert.ToString(f));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikowaAA.Sprint5.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.DewjaterikowaAA.Sprint5.Task0.V7.Lib;
+using System.IO;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint5.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void SaveToFileTextData()
+        {
+            //string path = @"C:\Пользователи\user\source\repos\Tyuiu.DewjaterikowaAA.Sprint5\Tyuiu.DewjaterikowaAA.Sprint5.Task0.V7\bin\Debug\OutPutFileTask0.txt";
+
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}

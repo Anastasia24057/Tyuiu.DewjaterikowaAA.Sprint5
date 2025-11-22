0711c36bf999c262e0f87eca3bbec2aee8c0434a
Anastasia24057 <nastya.dewa@gmail.com>
2025-11-22 21:13:34 +0500

    Task5

---
+++ Tyuiu.DewjaterikowaAA.Sprint5.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint5.Task5.V15.Lib
+{
+    public class DataService : ISprint5Task5V15
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string text = File.ReadAllText(path).Replace('.', ',');
+            string[] strings = text.Split(' ');
+
+            double minNumber = double.MaxValue;
+
+            foreach (string str in strings)
+            {
+                if (double.TryParse(str.Trim(), out double number))
+                {
+                    if (number % 5 == 0)
+                    {
+                        minNumber = number;
+                    }
+                }
+            }
+            return minNumber;
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikowaAA.Sprint5.Task5.V15/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikowaAA.Sprint5.Task5.V15.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #5 | Выполнила: Девятерикова А.А. | ИИПб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема:  Потоковый метод записи данных в бинарный файл                    *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #23                                                             *");
+Console.WriteLine("* Выполнил: Девятерикова А. А. | ИИПб-25-1                                *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле.            *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = @"C:\DataSprint5\InPutDataFileTask5V15.txt";
+
+
+Console.WriteLine("Данные находятся в файле: " + path);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+double res = ds.LoadFromDataFile(path);
+Console.WriteLine(res);
+Console.ReadKey();

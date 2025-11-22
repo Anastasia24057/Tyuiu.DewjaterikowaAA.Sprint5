72bb53657c29ce305a8440eb869ebb938c40ab82
Anastasia24057 <nastya.dewa@gmail.com>
2025-11-22 20:22:41 +0500

    Task4

---
+++ Tyuiu.DewjaterikowaAA.Sprint5.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DewjaterikowaAA.Sprint5.Task4.V23.Lib;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint5.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void LoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V23.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
